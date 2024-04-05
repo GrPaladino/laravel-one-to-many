fa92c8dc727ecf74a2bb0f29d4842cd87de7fe05
GrPaladino <paladino.graziano@gmail.com>
2024-04-05 15:30:05 +0200

    Feat: changed db name and seeded fake projects

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -7,6 +7,7 @@ use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -15,14 +16,22 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $project = new Project;
-        $project->title = 'js-campominato-grid';
-        $project->slug = Str::slug($project->title);
-        $project->description = 'Rappresentazione del mitico gioco Campo Minato, creato usando html, css e javascript.';
-        $project->github_url = 'https://github.com/GrPaladino/js-campominato-grid';
-        $project->image_preview = 'https://picsum.photos/200/300';
-        $project->save();
+
+        $types_id = Type::all()->pluck('id');
+
+        for ($i = 0; $i < 100; $i++) {
+
+            $project = new Project;
+            $project->title = $faker->catchPhrase();
+            $project->type_id = $faker->randomElement($types_id);
+            $project->slug = Str::slug($project->title);
+            $project->description = $faker->paragraph(3, true);
+            $project->github_url = 'https://github.com/GrPaladino';
+            $project->image_preview = 'https://picsum.photos/200/300';
+            $project->save();
+        }
+
     }
 }
